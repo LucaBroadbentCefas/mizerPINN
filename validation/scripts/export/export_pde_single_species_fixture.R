a37378d84fa84628c9b9c6f1d8116bcbc1605963
LucaBroadbentCefas <luke.broadbent@cefas.gov.uk>
2026-06-10 12:08:14 +0100

    Added r mizer and pinn comparison plot.

--- validation/scripts/export/export_pde_single_species_fixture.R
+++ validation/scripts/export/export_pde_single_species_fixture.R
@@ -13,7 +13,8 @@ export_mizer_inputs_for_python(
   n = params_1@initial_n,
   n_pp = params_1@initial_n_pp,
   dt = 0.1,
-  outdir = outdir
+  outdir = outdir,
+  t_max = 40
 )
 
 write_vec <- function(x, name) {
@@ -27,9 +28,10 @@ write_vec <- function(x, name) {
 write_vec(0, "t_min")
 write_vec(40, "t_max")
 
-
-
+#this is from getRDD - but should it have more w bins? It would be more accurate ?
 R_const <- 3711296885
+
+#I now have to calculate g0, then calculate it.
 
 sp <- species_params(params_1)
 sp$constant_reproduction <- R_const
@@ -62,5 +64,54 @@ ggplot(df, aes(x = x, y = log10_N, colour = time, group = time)) +
     colour = "time",
     title = "mizer log10(N) profiles through time"
   ) +
-  theme_bw()
+  theme_bw
+
+##plotting the other stuff on it.
+
+library(readr)
+df_pinn_all <- read_csv("runs/pde_only_single_species/20260609_134736/fixed_grid_diagnostics/fixed_grid_fields.csv")
+
+df_r <- data.frame(
+  time_num = rep(tt[ii], each = length(w)),
+  x = rep(x, times = length(ii)),
+  log10_N = as.vector(t(z)),
+  source = "R/mizer"
+)
+
+target_times <- tt[ii]
+
+nearest_pinn_times <- sapply(target_times, function(t0) {
+  df_pinn_all$t_eval[which.min(abs(df_pinn_all$t_eval - t0))]
+})
+
+df_pinn <- df_pinn_all %>%
+  filter(t_eval %in% nearest_pinn_times) %>%
+  transmute(
+    time_num = t_eval,
+    x = x_eval,
+    log10_N = log10_N,
+    source = "PINN"
+  )
+
+# Use common labels so R and PINN lines share colour by comparable time.
+# This maps each PINN time to the nearest selected R time label.
+df_pinn <- df_pinn %>%
+  rowwise() %>%
+  mutate(time_label = sprintf("%.2f", target_times[which.min(abs(target_times - time_num))])) %>%
+  ungroup()
+
+df_r <- df_r %>%
+  mutate(time_label = sprintf("%.2f", time_num))
+
+df_plot <- bind_rows(df_r, df_pinn)
+
+ggplot(df_plot, aes(x = x, y = log10_N, colour = time_label, linetype = source)) +
+  geom_line(linewidth = 0.8) +
+  labs(
+    x = "log weight",
+    y = "log10(N)",
+    colour = "time",
+    linetype = "source",
+    title = "R/mizer and PINN log10(N) profiles through time"
+  ) + theme_linedraw()
 
